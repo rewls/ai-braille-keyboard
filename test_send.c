a3e8c6a6d3554cdd02f973ae8b869e0001167284
koo001602 <52703286+koo001602@users.noreply.github.com>
2024-11-02 13:22:34 +0900

    Create test_send.c

---
+++ test_send.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <pthread.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "report.h"
+#include "jamo.h"
+#include "report.h"
+#include "braille.h"
+
+int main()
+{	
+    int braille_list[4] = {35, 18, 9, 59};
+    for(int i=0; i<4; i++)
+    {
+        send_keycode(braille_list[i]);
+        sleep(1);
+    }
+	remove_word(fd);
+	close(fd);
+	return 0;
+}
